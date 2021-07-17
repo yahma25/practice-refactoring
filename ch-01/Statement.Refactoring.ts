0d0d6e999e8a5578ab62cb4a92008c58eb211ec9
mhkim <yahma25@gmail.com>
2021-07-17 09:57:42 +0900

    #1 함수의 결과값을 명확하게 하기 위해 이름 변경

--- ch-01/Statement.Refactoring.ts
+++ ch-01/Statement.Refactoring.ts
@@ -39,27 +39,27 @@ export function statement(invoice: Invoice, plays: Plays): string {
  * - 값이 바뀌지 않는 변수는 매개변수로 전달
  */
 function amountFor(perf: Performance, play: Play): number {
-  let thisAmount = 0;
+  let result = 0; // 명확한 이름으로 변경. thisAmount -> result
 
   switch (play.type) {
     case 'tragedy':
-      thisAmount = 40000;
+      result = 40000;
       if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+        result += 1000 * (perf.audience - 30);
       }
       break;
 
     case 'comedy':
-      thisAmount = 30000;
+      result = 30000;
       if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+        result += 10000 + 500 * (perf.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * perf.audience;
       break;
 
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
   }
 
-  return thisAmount;
+  return result;
 }
