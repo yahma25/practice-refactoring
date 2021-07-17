86bdbaaf38f3d0072793a46d8ae4a94701feb66a
mhkim <yahma25@gmail.com>
2021-07-17 10:19:16 +0900

    #1 외부로 분리했던 function을 statement 내부로 이동
    호이스팅 방식으로 사용하는 것은 마음에 들지 않지만, 여기서 핵심은 아니므로 따라서 함

--- ch-01/Statement.Refactoring.ts
+++ ch-01/Statement.Refactoring.ts
@@ -10,10 +10,6 @@ export function statement(invoice: Invoice, plays: Plays): string {
     minimumFractionDigits: 2,
   }).format;
 
-  const playFor = (performance: Performance): Play => {
-    return plays[performance.playID];
-  };
-
   for (let perf of invoice.performances) {
     // 한 번의 공연에 대한 요금을 계산
     let thisAmount: number = amountFor(perf, playFor(perf));
@@ -34,35 +30,39 @@ export function statement(invoice: Invoice, plays: Plays): string {
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
-}
 
-/**
- * 개수 구하기
- * [point]
- * - 값이 바뀌지 않는 변수는 매개변수로 전달
- */
-function amountFor(performance: Performance, play: Play): number {
-  let result = 0; // 명확한 이름으로 변경. thisAmount -> result
+  /**
+   * 개수 구하기
+   * [point]
+   * - 값이 바뀌지 않는 변수는 매개변수로 전달
+   */
+  function amountFor(performance: Performance, play: Play): number {
+    let result = 0; // 명확한 이름으로 변경. thisAmount -> result
 
-  switch (play.type) {
-    case 'tragedy':
-      result = 40000;
-      if (performance.audience > 30) {
-        result += 1000 * (performance.audience - 30);
-      }
-      break;
+    switch (play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (performance.audience > 30) {
+          result += 1000 * (performance.audience - 30);
+        }
+        break;
 
-    case 'comedy':
-      result = 30000;
-      if (performance.audience > 20) {
-        result += 10000 + 500 * (performance.audience - 20);
-      }
-      result += 300 * performance.audience;
-      break;
+      case 'comedy':
+        result = 30000;
+        if (performance.audience > 20) {
+          result += 10000 + 500 * (performance.audience - 20);
+        }
+        result += 300 * performance.audience;
+        break;
 
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    return result;
   }
 
-  return result;
+  function playFor(performance: Performance): Play {
+    return plays[performance.playID];
+  }
 }
