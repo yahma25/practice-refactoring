2119dd7cda15fb3b8e8433bb64adc287d08e829c
mhkim <yahma25@gmail.com>
2021-07-17 09:52:16 +0900

    #1 statement 리팩터링 코드 추가, switch 코드 내용 별도의 함수로 분리

---
+++ ch-01/Statement.Refactoring.ts
@@ -0,0 +1,65 @@
+import { Invoice, Play, Plays, Performance } from './Types';
+
+export function statement(invoice: Invoice, plays: Plays): string {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+
+    // 한 번의 공연에 대한 요금을 계산
+    let thisAmount: number = amountFor(perf, play);
+
+    // 포인트를 적립한다.
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    // 청구 내역을 출력한다.
+    result += `${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += thisAmount;
+  }
+
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+}
+
+/**
+ * 개수 구하기
+ * [point]
+ * - 값이 바뀌지 않는 변수는 매개변수로 전달
+ */
+function amountFor(perf: Performance, play: Play): number {
+  let thisAmount = 0;
+
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+
+    case 'comedy':
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+
+  return thisAmount;
+}
