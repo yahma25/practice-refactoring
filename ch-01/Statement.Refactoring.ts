eb7ab4f745db33f816cad6423b3cca214848ab73
mhkim <yahma25@gmail.com>
2021-07-17 10:10:45 +0900

    #1 play 변수 줄이기

--- ch-01/Statement.Refactoring.ts
+++ ch-01/Statement.Refactoring.ts
@@ -10,19 +10,22 @@ export function statement(invoice: Invoice, plays: Plays): string {
     minimumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+  const playFor = (performance: Performance): Play => {
+    return plays[performance.playID];
+  };
 
+  for (let perf of invoice.performances) {
     // 한 번의 공연에 대한 요금을 계산
-    let thisAmount: number = amountFor(perf, play);
+    let thisAmount: number = amountFor(perf, playFor(perf));
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += `${play.name}: ${format(thisAmount / 100)} (${
+    result += `${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     }석)\n`;
     totalAmount += thisAmount;
