e008ddbeddbdcb5eb42c5dfa9cd1882c7caa5fec
mhkim <yahma25@gmail.com>
2021-07-19 23:26:42 +0900

    #1 data를 html 텍스트로 렌더링하는 함수 추가

--- ch-01/Statement.Refactoring.ts
+++ ch-01/Statement.Refactoring.ts
@@ -2,28 +2,47 @@ import { Invoice, Plays } from './Types';
 import { StatementData, createStatementData } from './createStatementData';
 
 export function statement(invoice: Invoice, plays: Plays): string {
-  const statementData: StatementData = createStatementData(invoice, plays);
-  return renderPlainText(statementData, plays);
+  return renderPlainText(createStatementData(invoice, plays));
+}
 
-  function renderPlainText(data: StatementData, plays: Plays): string {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
+function renderPlainText(data: StatementData): string {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-    for (let perf of data.performances) {
-      // 청구 내역을 출력한다.
-      result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-    }
-
-    result += `총액: ${usd(data.totalAmount)}\n`;
-    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-
-    return result;
+  for (let perf of data.performances) {
+    // 청구 내역을 출력한다.
+    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
 
-  function usd(aNumber: number): string {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+
+  return result;
+}
+
+function htmlStatement(invoice: Invoice, plays: Plays): string {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data: StatementData): string {
+  let result: string = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += '<table>\n';
+  result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>';
+
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
+  result += '</table>\n';
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+
+  return result;
+}
+
+function usd(aNumber: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
