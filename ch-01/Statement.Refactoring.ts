00d677cf304720b9e6346004eaf10fc81acd52c7
mhkim <yahma25@gmail.com>
2021-07-19 22:05:54 +0900

    #1 본문의 내용을 텍스트로 렌더링하는 함수로 분리

--- ch-01/Statement.Refactoring.ts
+++ ch-01/Statement.Refactoring.ts
@@ -1,18 +1,23 @@
 import { Invoice, Play, Plays, Performance } from './Types';
 
 export function statement(invoice: Invoice, plays: Plays): string {
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  return renderPlainText(invoice, plays);
 
-  for (let perf of invoice.performances) {
-    // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
+  function renderPlainText(invoice: Invoice, plays: Plays): string {
+    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
+    for (let perf of invoice.performances) {
+      // 청구 내역을 출력한다.
+      result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
+        perf.audience
+      }석)\n`;
+    }
+
+    result += `총액: ${usd(totalAmount())}\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+
+    return result;
   }
-
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
 
   function usd(aNumber: number): string {
     return new Intl.NumberFormat('en-US', {
