03aa85cb5b282f5504ebb966865c47664fafddd3
mhkim <yahma25@gmail.com>
2021-08-06 23:00:29 +0900

    #4 production 케이스 추가

--- ch-04/Province.test.ts
+++ ch-04/Province.test.ts
@@ -16,4 +16,10 @@ describe('province', () => {
   it('profit', () => {
     expect(asia.profit).toBe(230);
   });
+
+  it('change production', () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toBe(-6);
+    expect(asia.profit).toBe(292);
+  });
 });
